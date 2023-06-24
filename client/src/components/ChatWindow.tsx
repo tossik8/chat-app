a0a9acc5e98dd32e4c67160569b2458c6f109884
unknown <toropov.nikita2004@gmail.com>
2023-06-24 10:52:38 +0200

    feat: add an onKeyDown event for the send button

--- client/src/components/ChatWindow.tsx
+++ client/src/components/ChatWindow.tsx
@@ -19,14 +19,20 @@ const ChatWindow = ({client} : ChatWindowProps) => {
   }
 
   const handleClick = () => {
-    if(input){
+    if(input.trim()){
       setInput("")
+      document.getElementsByTagName("textarea")[0].style.height = "20px"
       client.current.publish({destination: "/app/message", body: JSON.stringify({chatId: id, from: username, text: input})})
     }
   }
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if(e.ctrlKey && e.key === "Enter"){
+      handleClick()
+    }
+  }
+  const handleButtonKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
+    if(e.key === " "){
       handleClick()
     }
   }
@@ -40,9 +46,9 @@ const ChatWindow = ({client} : ChatWindowProps) => {
             {users.length === 1? null : <p className="text-xs text-stone-400">{users.length} members</p>}
           </div>
           <ul id="messages" className="absolute bottom-20 px-4">{messages.map((message, i) => <li key={i}>{message.text}</li>)}</ul>
-          <div className="bg-white flex items-center absolute bottom-0 w-full p-4 border-l border-stone-300">
+          <div className="bg-white flex items-center absolute bottom-0 w-full px-4 py-2 border-l border-stone-300">
             <textarea onKeyDown={e => handleKeyDown(e)} onChange={e => handleChange(e)} className="placeholder:text-[0.9rem] h-5 max-h-28 overflow-hidden placeholder:italic resize-none w-full pr-5 focus:outline-0 focus:caret-blue-600" placeholder="Your message..." value={input} name="message"/>
-            <button className="transition-{scale} ease-in-out duration-75 hover:scale-110 focus-visible:outline-0 focus-visible:scale-110"><img src="/send.png" width={25} alt="Send icon." onClick={handleClick}/></button>
+            <button onKeyDown={e => handleButtonKeyDown(e)} onClick={handleClick} className="p-2 hover:bg-stone-200 focus-visible:outline-0 focus-visible:bg-stone-200 active:scale-90"><img src="/send.png" width={25} alt="Send icon."/></button>
           </div>
         </>}
     </main>
