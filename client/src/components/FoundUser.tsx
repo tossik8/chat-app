a8959cba82586348f329c9960b41fc72d11af0d5
unknown <toropov.nikita2004@gmail.com>
2023-07-22 13:28:03 +0200

    feat: determine if there is a chat with a found user

--- client/src/components/FoundUser.tsx
+++ client/src/components/FoundUser.tsx
@@ -1,7 +1,11 @@
 import { useDispatch } from "react-redux"
-import { setTitle, setId as setChatId, setMessages } from "../store/selectedChatSlice"
-import { displayLogo } from "./Chat"
+import { setTitle, setId as setChatId, setMessages, setUsers } from "../store/selectedChatSlice"
+import { changeActiveElementColour, displayLogo } from "./Chat"
 import { setId } from "../store/foundUsersSlice"
+import { useSelector } from "react-redux"
+import { RootState } from "../store/store"
+import { useEffect } from "react"
+import { IChat } from "../global/types"
 
 interface IFoundUser{
     id: number
@@ -13,15 +17,24 @@ const FoundUser = ({id, title, username} : IFoundUser) => {
     const activeStateColour = "bg-blue-200"
     const hoveredStateColour = "bg-stone-200"
     const dispatch = useDispatch()
+    const { chats } = useSelector((state : RootState) => state.user)
+    const { id: chatId } = useSelector((state: RootState) => state.selectedChat)
+    let chatsWithUser : IChat[] = []
     const handleClick = () => {
-        dispatch(setMessages([]))
-        dispatch(setChatId(id))
-        dispatch(setId(id))
+        chatsWithUser = chats.filter(chat => chat.connectedUsers.length === 1 && chat.connectedUsers[0].id === id)
+        if(chatsWithUser.length === 0){
+            dispatch(setMessages([]))
+            dispatch(setChatId(id))
+            dispatch(setId(id))
+        }
+        else{
+            dispatch(setMessages(JSON.parse(sessionStorage.getItem(`chat-${chatsWithUser[0].id}`)!)))
+            dispatch(setChatId(chatsWithUser[0].id))
+            dispatch(setUsers(chatsWithUser[0].connectedUsers))
+        }
         dispatch(setTitle(title))
-        const element = document.getElementById(id.toString()) as HTMLElement
         document.getElementsByClassName(activeStateColour)[0]?.classList.remove(activeStateColour)
-        element?.classList.remove(hoveredStateColour)
-        element?.classList.add(activeStateColour)
+        changeActiveElementColour(`${id}`)
     }
     const handleMouseOver = () => {
         const hoveredElement = document.getElementById(id.toString());
@@ -32,6 +45,12 @@ const FoundUser = ({id, title, username} : IFoundUser) => {
     const handleMouseLeave = () => {
         document.getElementById(id.toString())?.classList.remove(hoveredStateColour);
     }
+    useEffect(() => {
+        chatsWithUser = chats.filter(chat => chat.connectedUsers.length === 1 && chat.connectedUsers[0].id === id)
+        if(chatsWithUser[0]?.id === chatId){
+            changeActiveElementColour(`${id}`)
+        }
+    }, [])
     return (
         <article id={id.toString()} onClick={handleClick} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave} className="flex min-h-[4rem] items-center px-4 py-2 cursor-pointer gap-4">
             <div className="min-w-[3rem] min-h-[3rem] flex justify-center items-center text-white bg-gradient-to-b from-cyan-500 to-blue-500 font-medium rounded-3xl text-xl select-none">{displayLogo(title)}</div>
