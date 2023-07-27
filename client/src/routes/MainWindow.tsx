6c7fbc3facdca0a298f12b1efd99d69f575a8fb9
unknown <toropov.nikita2004@gmail.com>
2023-07-27 11:10:47 +0200

    fix: set a name for all chats on first load

--- client/src/routes/MainWindow.tsx
+++ client/src/routes/MainWindow.tsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react"
 import ChatService from "../services/ChatService"
 import { useDispatch } from "react-redux"
 import { setChats } from "../store/userSlice"
+import { IChat } from "../global/types"
 
 
 const MainWindow = () => {
@@ -18,6 +19,11 @@ const MainWindow = () => {
     const dispatch = useDispatch()
     useEffect(() => {
         ChatService.getChats(id).then(message => {
+            message.data.forEach((chat: IChat) => {
+                if(!chat.name){
+                    chat.name = `${chat.connectedUsers[0].name} ${chat.connectedUsers[0].surname}`
+                }
+            })
             dispatch(setChats(message.data))
         })
     }, [])
@@ -31,13 +37,10 @@ const MainWindow = () => {
                 <div>
                     <Navigation/>
                     <div className="max-h-[1400px] h-[94.59vh] overflow-y-auto scrollbar-thin scrollbar-track-gray-500 scrollbar-thumb-gray-700">
-                        {foundUsers.length !== 0? foundUsers.map(foundUser => <FoundUser key={foundUser.id} id={foundUser.id} title={`${foundUser.name} ${foundUser.surname}`} username={foundUser.username} setIsChatWindow={setIsChatWindow} width={width}/>) : chats.map(chat => {
-                            const users = chat.connectedUsers
-                            if(chat.name){
-                                return <Chat key={chat.id} id={chat.id} title={chat.name} connectedUsers={users} messages={chat.messages} setIsChatWindow={setIsChatWindow} width={width}/>
-                            }
-                            return <Chat key={chat.id} id={chat.id} title={`${users[0]!.name} ${users[0]!.surname}`} connectedUsers={users} messages={chat.messages} setIsChatWindow={setIsChatWindow} width={width}/>
-                        })}
+                        {foundUsers.length !== 0? foundUsers.map(foundUser => <FoundUser key={foundUser.id} id={foundUser.id} title={`${foundUser.name} ${foundUser.surname}`} username={foundUser.username} setIsChatWindow={setIsChatWindow} width={width}/>)
+                        :
+                        chats.map(chat => <Chat key={chat.id} id={chat.id} title={chat.name} connectedUsers={chat.connectedUsers} messages={chat.messages} setIsChatWindow={setIsChatWindow} width={width}/>
+                        )}
                     </div>
                 </div>
                 <ChatWindow width={width} setIsChatWindow={setIsChatWindow}/>
@@ -46,13 +49,10 @@ const MainWindow = () => {
             <>
                 <Navigation/>
                 <div className="max-h-[1400px] h-[94.59vh] overflow-y-auto scrollbar-thin scrollbar-track-gray-500 scrollbar-thumb-gray-700">
-                    {foundUsers.length !== 0? foundUsers.map(foundUser => <FoundUser key={foundUser.id} id={foundUser.id} title={`${foundUser.name} ${foundUser.surname}`} username={foundUser.username} setIsChatWindow={setIsChatWindow} width={width}/>) : chats.map(chat => {
-                        const users = chat.connectedUsers
-                        if(chat.name){
-                            return <Chat key={chat.id} id={chat.id} title={chat.name} connectedUsers={users} messages={chat.messages} setIsChatWindow={setIsChatWindow} width={width}/>
-                        }
-                        return <Chat key={chat.id} id={chat.id} title={`${users[0]!.name} ${users[0]!.surname}`} connectedUsers={users} messages={chat.messages} setIsChatWindow={setIsChatWindow} width={width}/>
-                    })}
+                    {foundUsers.length !== 0? foundUsers.map(foundUser => <FoundUser key={foundUser.id} id={foundUser.id} title={`${foundUser.name} ${foundUser.surname}`} username={foundUser.username} setIsChatWindow={setIsChatWindow} width={width}/>)
+                    :
+                    chats.map(chat => <Chat key={chat.id} id={chat.id} title={chat.name} connectedUsers={chat.connectedUsers} messages={chat.messages} setIsChatWindow={setIsChatWindow} width={width}/>
+                    )}
                 </div>
             </>
             }
